dc650b67419c272f92c0a2e81567d79f62696d8e
teamtrainit <teamtrainit@gmail.com>
2022-04-23 10:42:56 +0700

    buat baris coding kedua index dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,5 +8,9 @@ class DashboardController extends Controller
 {
     function index(){
         echo "saya halaman dashboard";
+
+
+        echo "<h1> ini adlah coding kedua saya pada dashboard</h1>";
+        echo "<p>harapannya tercatatan coding pertama dan kedua ini</p>";
     }
 }
