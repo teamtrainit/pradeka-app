8ec435d10c48835e647c2b74ae06a90dfb112944
teamtrainit <teamtrainit@gmail.com>
2022-04-23 11:05:54 +0700

    buang paragraf pada dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,11 +6,9 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    function index(){
+    function index() {
         echo "saya halaman dashboard";
-
-
         echo "<h1> ini adlah coding kedua saya pada dashboard</h1>";
-        echo "<p>harapannya tercatatan coding pertama dan kedua ini</p>";
+        
     }
 }
